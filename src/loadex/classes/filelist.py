d05712754417abd240adaf39b8176310a67e6049
Cameron Brown <cab@goldwinddenmark.com>
2025-10-10 11:56:26 +0200

    release with standard weio package

--- src/loadex/classes/filelist.py
+++ src/loadex/classes/filelist.py
@@ -1,17 +1,17 @@
 from loadex.classes.dataset import DataSet
 import pandas as pd
 from pathlib import Path
+from typing import List, Dict
 
 
 class File(object):
     """Contains a file from a loads dataset"""
 
-    def __init__(self,parent:DataSet, filepath: str):
-        self.parent=parent
+    def __init__(self, filepath: str):
         self.filepath = filepath
         self.metadata = dict()
 
-    def read(self):
+    def read(self)->pd.DataFrame:
         """Read the file and return a DataFrame"""
         data_file = self.parent.format(filename=self.filepath)
         return data_file.toDataFrame()
@@ -19,3 +19,15 @@ class File(object):
     def __repr__(self):
         return f"File({self.filepath})"
 
+class FileList(object):
+    """Contains a list of files from a loads dataset"""
+
+    def __init__(self,parent:DataSet,list:List[File]=[],metadata:Dict=dict()):
+        self.parent=parent
+        self.list = list
+        self.metadata = metadata
+
+
+    
+    def __repr__(self):
+        return f"File({self.filepath})"
