3053625544af9dfda31fcd98c487f03b13be85c6
Cameron Brown <cab@goldwinddenmark.com>
2026-03-03 13:47:55 +0100

    allow for status update to go to function instead of logfile

--- src/loadex/cli/process_one_file.py
+++ src/loadex/cli/process_one_file.py
@@ -15,19 +15,20 @@ def log_file_path(file_path:Path|str)->Path:
         file_path=Path(file_path)
     return file_path.with_suffix('.loadex_log')
 
-def process_one_file(file_path: str,db_file:str=None,file_format:str="BladedOutFile",fatigue_sensor_spec:list[dict]=None):
+def status_to_logfile(progress:int, message:str, log_file:Path):
+    with open(log_file,'w') as f:
+        f.write(f'{progress}%\t{message}\n')
+
+def process_one_file(file_path: str,db_file:str=None,file_format:str="BladedOutFile",fatigue_sensor_spec:list[dict]=None,update_log:function=None):
     file_path=Path(file_path)
     if not file_path.exists():
         warnings.warn(f"File not found: {file_path}", UserWarning)
 
-    log_file= log_file_path(file_path)
-    log_file.unlink(missing_ok=True)
+    if update_log is None:
+        log_file= log_file_path(file_path)
+        log_file.unlink(missing_ok=True)
+        update_log = lambda progress, message: status_to_logfile(progress, message, log_file)
 
-    def update_log(progress:int, message:str):
-        with open(log_file,'w') as f:
-            f.write(f'{progress}%\t{message}\n')
-
-    
     if not db_file:
         db_file="..//statistics.db"
     db_file=file_path.parent / db_file
